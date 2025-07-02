4d7c19247fe392a30c193756f07cd87942b7b9e1
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-07-02 14:36:31 -0400

    Fix

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -20,7 +20,6 @@ const submit = () => {
 <template>
     <GuestLayout>
         <Head title="Confirm Password" />
-
         <div class="mb-4 text-sm text-gray-600">
             This is a secure area of the application. Please confirm your password before continuing.
         </div>
